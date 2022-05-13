ace5f501a513b8f80580b2ea530797c3047dbe20
karltayeb <karl.tayeb7@gmail.com>
2022-05-13 14:04:08 -0500

    wrapper for logistic susie

--- R/logistic_susie_veb.R
+++ R/logistic_susie_veb.R
@@ -1,20 +1,20 @@
 #' wrapper for VEB.boost to fit logistic susie
 #' and tidy up output to be consistent with other susie-type outputs
 #' @export
-fit_logistic_susie_veb_boost = function(X, y, L=10){
+fit_logistic_susie_veb_boost = function(X, y, L=10, ...){
     veb.fit = VEB.Boost::veb_boost_stumps(
         X, y, family = 'binomial',
         include_stumps=FALSE,
         max_log_prior_var=35,
         scale_X = 'NA',
-        growTree=FALSE,
+        growMode = 'NA',
         changeToConstant=F,
-        k=L
+        k=L, ...
     )
 
-    alpha <- t(do.call(cbind, lapply(veb.fit$leaves, function(x) x$currentFit$alpha)))
-    mu <- t(do.call(cbind, lapply(veb.fit$leaves, function(x) x$currentFit$mu)))
-    mu2 <- t(do.call(cbind, lapply(veb.fit$leaves, function(x) x$currentFit$mu2)))
+    alpha <- t(do.call(cbind, lapply(veb.fit$leaves, function(x) x$learner$currentFit$alpha)))
+    mu <- t(do.call(cbind, lapply(veb.fit$leaves, function(x) x$learner$currentFit$mu)))
+    mu2 <- t(do.call(cbind, lapply(veb.fit$leaves, function(x) x$learner$currentFit$mu2)))
     elbo <- veb.fit$ELBO_progress[[2]]
     res <- list(alpha=alpha, mu=mu, mu2=mu2, elbo=elbo, veb.fit=veb.fit)
     class(res) <- 'susie'
