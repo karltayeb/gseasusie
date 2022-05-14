ff30effd444be32c4745ed83cc7850fdafdeb385
karltayeb <karl.tayeb7@gmail.com>
2022-05-14 11:57:30 -0500

    meta load functions

--- R/load_genesets.R
+++ R/load_genesets.R
@@ -163,7 +163,6 @@ load_msigdb_geneset_x <- function(db='C2', min.size=10){
 #' load gene sets from various sources with uniform format
 #' @export
 load_gene_sets = function(dbs=c('gobp', 'gobp_nr', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'pathways')) {
-  load_pathways_genesets <- load_pathways_genesets
   promise <- tibble::tribble(
     ~name, ~expression,
     'gobp', rlang::expr(gseareg:::load_webgestalt_geneset_x('geneontology_Biological_Process', min.size=50)),
@@ -177,7 +176,9 @@ load_gene_sets = function(dbs=c('gobp', 'gobp_nr', 'c1', 'c2', 'c3', 'c4', 'c5',
     'c4', rlang::expr(gseareg:::load_msigdb_geneset_x('C4', min.size=1)),
     'c5', rlang::expr(gseareg:::load_msigdb_geneset_x('C5', min.size=1)),
     'c6', rlang::expr(gseareg:::load_msigdb_geneset_x('C6', min.size=1)),
-    'pathways', rlang::expr(gseareg:::load_pathways_genesets())
+    'pathways', rlang::expr(gseareg:::load_pathways_genesets()),
+    'all_msigdb', rlang::expr(gseareg:::load_all_msigdb()),
+    'all_go', rlang:::expr(gseareg:::load_all_go())
   )
   genesets <-
     promise %>%
@@ -189,15 +190,35 @@ load_gene_sets = function(dbs=c('gobp', 'gobp_nr', 'c1', 'c2', 'c3', 'c4', 'c5',
 }
 
 #' take a list of genesets and concatenate them
+#' @param genesets a list of gene sets like the output from load_gene_sets
+#' @param min.size minimum size of gene set for inclusion
+#' @param name a name for this geneset (it get's cached for fast loading in the future)
 #' @export
-concat_genesets = function(genesets){
-  # intersect/union of genes
-  all_genes <- map(genesets, ~ .x$X %>% rownames())
-  common_genes <- Reduce(intersect, all_genes)
-  union_genes <- unique(unlist(all_genes))
+concat_genesets = function(genesets, min.size, name){
+  res <- xfun::cache_rds({
+    geneSet <- purrr::map_dfr(genesets, ~purrr::pluck(.x, 'geneSet', 'geneSet')) %>% dplyr::distinct()
+    geneSetDes <- purrr::map_dfr(genesets, ~purrr::pluck(.x, 'geneSet', 'geneSetDes')) %>% dplyr::distinct()
+    gs = list(geneSet = geneSet, geneSetDes = geneSetDes)
+    X <- gs %>% convertGeneSet(min.size= min.size) %>% geneSet2X
+    list(geneSet = gs, X=X, db=name, min.size=min.size)
+  }, dir='cache/resources/', file=paste0(name, '.', min.size, '.X.rds'))
+  return(res)
+}
 
-  # subset and order (how to make this work for union?)
-  X_sub <- map(genesets, ~with(.x, X[rownames(X) %in% common_genes, ] %>% {.[order(rownames(.)),]}))
+#' convenient function to load MSigDb C1-6
+#' @param min.size minimum number of genes in term
+#' @export
+load_all_msigdb = function(min.size=10){
+  genesets <- load_gene_sets(c('c1', 'c2', 'c3', 'c4', 'c5', 'c6'))
+  gs <- concat_genesets(genesets, min.size, 'all_msigdb')
+  return(gs)
+}
 
-  return(genesets)
+#' convenient function to load all GO terms (across ontologies BP, MF, CC)
+#' @param min.size minimum number of genes in term
+#' @export
+load_all_go = function(min.size=10){
+genesets <- gseareg::load_gene_sets(c('gobp', 'gomf', 'gocc'))
+go.gs <- concat_genesets(genesets, min.size, 'all_go')
+  return(gs)
 }
