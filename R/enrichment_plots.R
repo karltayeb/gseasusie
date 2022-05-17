95921487964a83f9828a732acb56b608fbf1e29b
karltayeb <karl.tayeb7@gmail.com>
2022-05-17 09:04:44 -0500

    add plot functions that take res as argument

--- R/enrichment_plots.R
+++ R/enrichment_plots.R
@@ -49,3 +49,27 @@ residual_enrichment_histogram = function(marginal_regression, residual_regressio
   plotdat %>% ggplot(aes(x=pval)) +
     ggplot2::geom_histogram() + ggplot2::facet_wrap(vars(model))
 }
+
+#' @export
+enrichment_volcano2 = function(res){
+  res %>% 
+    label_sig_enrichments() %>%
+    ggplot2::ggplot(aes(x=log10(oddsRatio), y=-log10(pFishersExact), color=result)) +
+    ggplot2::geom_point() +
+    ggplot2::geom_point(
+      res %>% filter(in_cs, active_cs), 
+      mapping=aes(x=log10(oddsRatio), y=-log10(pFishersExact), fill=component),
+      color='black', pch=21, size=5) +
+    ggplot2::scale_color_manual(values = c('depleted' = 'coral',
+                                           'enriched' = 'dodgerblue',
+                                           'not significant' = 'grey'))
+}
+
+#' @export
+residual_enrichment_histogram2 = function(res){
+  res %>%
+    dplyr::select(geneSet, pval_marginal, pval_residual) %>%
+    tidyr::pivot_longer(dplyr::starts_with('pval'), values_to = 'pval') %>%
+    ggplot2::ggplot(aes(x=pval)) +
+      ggplot2::geom_histogram() + ggplot2::facet_wrap(vars(name))
+}
