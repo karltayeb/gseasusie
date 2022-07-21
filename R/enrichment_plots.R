81aed6d356f628b757266a5f23aa6f06c5c5d491
karltayeb <karl.tayeb7@gmail.com>
2022-07-21 14:32:39 -0500

    pass column names to plot function

--- R/enrichment_plots.R
+++ R/enrichment_plots.R
@@ -1,9 +1,13 @@
-label_sig_enrichments = function(tbl){
+#' apply FDR correction to marginal pvalues and label enrichments "depleted" or "enriched"
+#' @param tbl a table containing columns corresponding to the other two arguments
+#' @param p character name of column with p values
+#' @param or character name of column with odds ratios/effect sizes
+label_sig_enrichments = function(tbl, p, or){
    tbl %>% dplyr::mutate(
-    padj = p.adjust(pFishersExact),
+    padj = p.adjust(!!sym(p), 'fdr'),
     result = dplyr::case_when(
-      padj < 0.05 & oddsRatio < 1 ~ 'depleted',
-      padj < 0.05 & oddsRatio > 1 ~ 'enriched',
+      padj < 0.05 & !!sym(or) < 1 ~ 'depleted',
+      padj < 0.05 & !!sym(or) > 1 ~ 'enriched',
       TRUE ~ 'not significant'
     )
   )
@@ -14,25 +18,23 @@ label_sig_enrichments = function(tbl){
 #' @param fit a fit logistic susie
 #' @param ora output from `fit_ora`
 #' @export
-enrichment_volcano = function(fit, ora){
-  res <- get_gene_set_summary(fit) %>%
-    dplyr::left_join(ora)
+enrichment_volcano = function (fit, ora, or='oddsRatio', p='pFishersExact'){
+  sym_p <- sym(p)
+  sym_or <- sym(or)
   
-  csdat <- get_credible_set_summary(fit) %>%
-    dplyr::left_join(ora) %>%
+  res <- get_gene_set_summary(fit) %>% dplyr::left_join(ora)
+  csdat <- get_credible_set_summary(fit) %>% dplyr::left_join(ora) %>% 
     dplyr::filter(in_cs, active_cs)
-  
   res %>% 
-    label_sig_enrichments() %>%
-    ggplot2::ggplot(aes(x=log10(oddsRatio), y=-log10(pFishersExact), color=result)) +
-    ggplot2::geom_point() +
-    ggplot2::geom_point(
-      csdat, 
-      mapping=aes(x=log10(oddsRatio), y=-log10(pFishersExact), fill=component),
-      color='black', pch=21, size=5) +
-    ggplot2::scale_color_manual(values = c('depleted' = 'coral',
-                                           'enriched' = 'dodgerblue',
-                                           'not significant' = 'grey'))
+    label_sig_enrichments(p=p, or=or) %>% 
+    ggplot2::ggplot(aes(x = log10(!!sym_or), y = -log10(!!sym_p), color = result)) +
+    ggplot2::geom_point() + 
+    ggplot2::geom_point(csdat, mapping = aes(x = log10(!!sym_or), 
+      y = -log10(!!sym_p), fill = component), color = "black", 
+      pch = 21, size = 5) + ggplot2::scale_color_manual(values = c(
+        depleted = "coral",
+        enriched = "dodgerblue",
+        `not significant` = "grey"))
 }
 
 #' @export
@@ -51,14 +53,14 @@ residual_enrichment_histogram = function(marginal_regression, residual_regressio
 }
 
 #' @export
-enrichment_volcano2 = function(res){
+enrichment_volcano2 = function(res, p='pFishersExact', or='oddsRatio'){
   res %>% 
     label_sig_enrichments() %>%
-    ggplot2::ggplot(aes(x=log10(oddsRatio), y=-log10(pFishersExact), color=result)) +
+    ggplot2::ggplot(aes(x=log10(!!sym(or)), y=-log10(!!sym(p)), color=result)) +
     ggplot2::geom_point() +
     ggplot2::geom_point(
       res %>% filter(in_cs, active_cs), 
-      mapping=aes(x=log10(oddsRatio), y=-log10(pFishersExact), fill=component),
+      mapping=aes(x=log10(!!sym(or)), y=-log10(!!sym(p)), fill=component),
       color='black', pch=21, size=5) +
     ggplot2::scale_color_manual(values = c('depleted' = 'coral',
                                            'enriched' = 'dodgerblue',
