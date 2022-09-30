96526c23c1102ee22c23c02bcbe6382428a88739
Karl Tayeb <karl.tayeb7@gmail.com>
2022-09-30 09:04:02 -0500

    test univariate vb

---
+++ tests/testthat/test-fit_univariate_vb_regression_jax.R
@@ -0,0 +1,11 @@
+test_that( "True effect has smallest ELBO", {
+  n <- 1000
+  p <- 10
+  X <- matrix(rnorm(n*p), nrow = n)
+  y <- rbinom(n, 1, 1/(1+exp(-X[,1])))
+
+  tictoc::tic()
+  res <- fit_univariate_vb_regression_jax(X, y, 1)
+  tictoc::toc()
+  expect_equal(which.max(res$elbo), 1)
+})
