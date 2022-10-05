6226309f3a6b7799c01d8bb0dcf9e44e2b543676
Karl Tayeb <karl.tayeb7@gmail.com>
2022-10-05 08:40:35 -0500

    add quadrture function

--- R/residual_regression.R
+++ R/residual_regression.R
@@ -138,9 +138,77 @@ fit_residual_regression_jax = function(X, y, fit, stride=1000){
 #'  @param tau0 the prior precision of the effect
 #'  @export
 fit_univariate_vb_regression_jax = function(X, y, tau0){
-  proc <- basilisk::basiliskStart(jax_env)
-  on.exit(basilisk::basiliskStop(proc))
-
   res <- .fit_univariate_vb_jax(X, y, 1.0, proc)
   return(res)
 }
+
+.compute_evidence_quadrature_jax = function(dat, proc){
+  mpy <- basilisk::basiliskRun(
+    proc, function(dat) {
+      np <- reticulate::import("numpy")
+      jnp <- reticulate::import("jax.numpy")
+
+      reticulate::source_python(system.file("python", "logistic_regression_quadrature.py", package = "gseasusie"))
+      mpy <- with(dat, np$array(logreg_quad_X(
+        jnp$array(X), jnp$array(y), b_mu, b_sigma, b0_mu, b0_sigma, jnp$array(nodes), jnp$array(weights))))
+      mpy
+    }, dat=dat
+  )
+  return(mpy)
+}
+
+
+#' Compute evidence for a univariate Bayesian logistic regression
+#' y ~ Binomial(sigmoid(x*b + b0)), b ~ N(b_mu, b_sigma^2), b0 ~ N(b0_mu, b0_sigma^2)
+#' @param X an n x p matrix of covariates
+#' @param y a binary n vector of responses
+#' @param b_mu prior mean for the effect
+#' @param b_sigma prior standard deviation of effect
+#' @param b0_mu prior mean for the intercept
+#' @param b0_sigma prior standard deviation of the intercept
+#' @param n the number of quadrature points in a Gauss Hermite quadrature
+#' @param stride the number of columns of X to batch together-- larger the better as long as you have memory to handle it
+compute_evidence_quadrature_jax = function(X, y, b_mu, b_sigma, b0_mu, b0_sigma, n=128, stride = 50){
+  proc <- basilisk::basiliskStart(jax_env)
+  on.exit(basilisk::basiliskStop(proc))
+
+  message('computing evidence via quadrature...')
+  tictoc::tic()
+
+  # get quadrature points
+  q <- statmod::gauss.quad.prob(n, dist='normal')
+  nodes <- q$nodes
+  weights <- q$weights
+
+  # construct strides-- break it up because the n x p x quadrature points
+  # is memory intensive (so we chunk up the p dimension)
+  p = dim(X)[2]
+  start_idx = seq(1, p, stride)
+  end_idx = pmin(start_idx + stride - 1, p)
+
+  # helper function makes a subset of the data for each stried
+  make_dat <- function(start_idx, end_idx){
+    dat <- list(
+      X=X[, start_idx:end_idx],
+      y=y + 1e-10,
+      b_mu = b_mu + 1e-10,
+      b_sigma = b_sigma + 1e-10,
+      b0_mu = b0_mu + 1e-10,
+      b0_sigma=b0_sigma + 1e-10,
+      nodes = nodes,
+      weights = weights
+    )
+    return(dat)
+  }
+
+  # for each stride compute log evidence
+  mpy <- unlist(purrr::map(
+    1:length(start_idx), ~.compute_evidence_quadrature_jax(
+      make_dat(start_idx[.x],end_idx[.x]), proc)
+  ))
+  tictoc::toc()
+  return(mpy)
+}
+
+
+
